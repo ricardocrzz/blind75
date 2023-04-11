f85d840d62418ff34736687e60b34b7a730b89ee
Ricardo Cruz <130194380+ricardocrzz@users.noreply.github.com>
2023-04-10 23:58:18 -0600

    Create 3twoSum.js

---
+++ 3twoSum.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const set = new Set()
+    for ( let i in nums) {
+        if (set.has(target - nums[i])) return [i, nums.indexOf(target - nums[i])]
+        set.add(nums[i])
+    }
+};
+/*  USING HASH SET IN ONE PASS O(N)
+var twoSum = function(nums, target) {
+    const set = new Set()
+    for ( let i in nums) {
+        if (set.has(target - nums[i])) return [i, nums.indexOf(target - nums[i])]
+        set.add(nums[i])
+    }
+};
+*/
+/* BRUTE FORCE O(N^2)
+var twoSum = function(nums, target) {
+    for (let i = 0; i< nums.length; i++) {
+        for (let j = i+1; j< nums.length; j++) {
+            if((nums[i]+nums[j])==target) {
+                return [i,j]
+            }
+        }
+    }
+}; 
+ */
